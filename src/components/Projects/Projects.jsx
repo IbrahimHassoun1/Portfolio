afea890b040c119cb0a38bd67bdfcfc126c53406
Ibrahim Hassoun <ihassoun73@gmail.com>
2024-11-16 10:59:59 -0800

    added second project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,7 +12,14 @@ const Projects = () => {
         <SlideUpSection className="text-5xl my-9 mx-auto font-bold flex justify-center items-center h-8">PROJECTS <Lottie  animationData={animationData} loop={true} className="h-20"/></SlideUpSection>
         
         <table className="min-h-72 w-full  flex flex-wrap justify-around mt-3">
-            <ProjectCard direction="left" time={100} videoSrc="/tl_alrabaa.mp4" description="This is a real-life e-commerce project" title="Tl-Alrabaa" link="https://tl-alrabaa.vercel.app/" code="https://github.com/IbrahimHassoun1/Tl-Alrabaa" features={["Product Catalog","Category & Filters","User Login System","Shopping Cart","Admin Panel","Order History & Tracking","Product Management"]} technologies={[
+            <ProjectCard direction="left" time={100} videoSrc="/tl_alrabaa.mp4" description="This is a real-life e-commerce project designed for Tl-Alrabba tobacco company" title="Tl-Alrabaa" link="https://tl-alrabaa.vercel.app/" code="https://github.com/IbrahimHassoun1/Tl-Alrabaa" features={["Product Catalog","Category & Filters","User Login System","Shopping Cart","Admin Panel","Order History & Tracking","Product Management"]} technologies={[
+    
+    { name: "React", icon: faReact },
+    { name: "JavaScript", icon: faJsSquare },
+    { name: "Node.js", icon: faNodeJs },
+    {name:"MongoDB",icon:faDatabase}
+  ]}/>
+  <ProjectCard direction="left" time={100} videoSrc="/Food delivery.mp4" description="This is a full stack practice project for a food delivery website" title="Tomato" link="https://tomato-production-front.vercel.app/" code="https://github.com/IbrahimHassoun1/tomato-production-front" features={["Online Payment","Shopping Cart","User Login System","Product Catalog","Category & Filters","Admin Panel","Order History & Tracking","Product Management"]} technologies={[
     
     { name: "React", icon: faReact },
     { name: "JavaScript", icon: faJsSquare },
